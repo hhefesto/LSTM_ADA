de48c2ba90fc5c307664340e5a85fc6e47092ec9
hhefesto <hhefesto@rdataa.com>
2018-11-08 13:38:53 -0600

    stash

--- init.sh
+++ init.sh
@@ -7,6 +7,5 @@ python3 -m venv myenv
 source /myenv/bin/activate # esto se tiene que hacer con windows también, pero que no funciona source
 #.\myenv\Scripts\activate # lo mismo pero para windows
 # curl https://bootstrap.pypa.io/get-pip.py | python # esto va con lo de --without-pip
-# myenv/bin/activate 
 pip install -r requirements.txt
 # put "deactivate" on the terminal to deactivate the virtual enviroment
