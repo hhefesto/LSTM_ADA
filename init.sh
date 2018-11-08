a53fc318ce9447567c8c1993dddb265fd01d9e14
hhefesto <hhefesto@rdataa.com>
2018-11-08 13:26:32 -0600

    no pude hacer que funcione en la compu del cinves

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
+# use python 3.6. python 3.7 crashed on windows
+
 sudo python3 -m pip install --upgrade pip
 python3 -m venv myenv
 # python3 -m venv myenv --without-pip
-source /myenv/bin/activate # esto se tiene que hacer con windows también, pero que no funciona source
 # Set-ExecutionPolicy Unrestricted # esto sólo se usa en windows
+source /myenv/bin/activate # esto se tiene que hacer con windows también, pero que no funciona source
+#.\myenv\Scripts\activate # lo mismo pero para windows
 # curl https://bootstrap.pypa.io/get-pip.py | python # esto va con lo de --without-pip
-# myenv/bin/activate
+# myenv/bin/activate 
 pip install -r requirements.txt
 # put "deactivate" on the terminal to deactivate the virtual enviroment
